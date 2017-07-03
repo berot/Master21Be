65f777fe7f079b10e9c606e7f0118ec9050acc95
Bettina Rotzetter <roetzeli@Ravenyard.local>
2017-07-03 20:18:03 +0200

    change line added the man

--- angreyboss.rb
+++ angreyboss.rb
@@ -6,4 +6,4 @@ names = ['johnson', 'smith', 'weinberg', 'filmore']
 puts "Can\'t you see I\'m busy? Make it fast, #{names[rand (4)].upcase}!"
 request = gets.chomp
 puts "WHADDAYA MEAN \"#{request.upcase}\"?!? YOU'RE FIRED!!"
-`say "What do you MEAN #{request.upcase}?!? YOU'RE FIRED!!"`
+`say "What do you MEAN #{request.upcase}?!? YOU'RE FIRED MAN!!"`
